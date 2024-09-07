47288852dcb1785063b251678e691de1e434ad60
timab <timabondarcuk@gmail.com>
2024-09-07 22:01:57 +0200

    createdSpring

--- src/main/java/com/example/sneakershop/SneakershopApplication.java
+++ src/main/java/com/example/sneakershop/SneakershopApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SneakershopApplication {
-
+//hello
 	public static void main(String[] args) {
 		SpringApplication.run(SneakershopApplication.class, args);
 	}
