713aa6a53087ebe93381d9f0e935d4b32b1fe2a1
timabondarcuk <timabondarcuk@gmail.com>
2025-02-07 06:21:43 +0100

    included file type

--- src/main/java/com/example/sneakershop/s3/S3Service.java
+++ src/main/java/com/example/sneakershop/s3/S3Service.java
@@ -8,12 +8,15 @@ import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
 import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
 
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.Duration;
 
 @Service
@@ -38,9 +41,14 @@ public class S3Service {
     public String uploadFile(String fileName, byte[] fileData) {
         String fileKey = "products/" + fileName;
 
+        // Detect Content-Type (or set it manually if known)
+        String contentType = detectContentType(fileName);
+
         PutObjectRequest putObjectRequest = PutObjectRequest.builder()
                 .bucket(bucketName)
                 .key(fileKey)
+                .contentType(contentType) // Set correct Content-Type
+                .acl(ObjectCannedACL.PUBLIC_READ) // Make it public if needed
                 .build();
 
         s3Client.putObject(putObjectRequest, RequestBody.fromBytes(fileData));
@@ -48,5 +56,13 @@ public class S3Service {
         return "https://" + bucketName + ".s3.amazonaws.com/" + fileKey;
     }
 
+    // Utility method to detect content type
+    private String detectContentType(String fileName) {
+        try {
+            return Files.probeContentType(Paths.get(fileName));
+        } catch (Exception e) {
+            return "application/octet-stream"; // Fallback if detection fails
+        }
 
+    }
 }
